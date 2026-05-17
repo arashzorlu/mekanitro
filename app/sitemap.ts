c74b51ed5c652a92b47671ba2c326530c683dd7b
Arash <arash.mach@gmail.com>
2026-05-17 23:16:07 +0300

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,44 @@
+export default function sitemap() {
+    return [
+      {
+        url: "https://mekanitrochiptuning.com",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://mekanitrochiptuning.com/stage-yazilim",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://mekanitrochiptuning.com/dsg-optimizasyonu",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://mekanitrochiptuning.com/dpf-egr-cozumu",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://mekanitrochiptuning.com/adblue-cozumu",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://mekanitrochiptuning.com/gpf-cozumu",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://mekanitrochiptuning.com/gizli-ozellik",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://mekanitrochiptuning.com/servis-hizmetleri",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://mekanitrochiptuning.com/mobil-destek",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://mekanitrochiptuning.com/blog",
+        lastModified: new Date(),
+      },
+    ]
+  }
