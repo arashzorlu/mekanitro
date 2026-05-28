151f78533a0f7144365d2fb51e6711acbcc5476e
Arash <arash.mach@gmail.com>
2026-05-28 22:16:50 +0300

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,7 +32,26 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+<body className="min-h-full flex flex-col">
+
+<Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-T6MTZCMJ13"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-T6MTZCMJ13');
+  `}
+</Script>
+
+{children}
+
+</body>
     </html>
   );
 }
